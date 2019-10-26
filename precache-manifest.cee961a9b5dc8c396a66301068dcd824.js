73e4d348a1061c9e60f585c56d3d1ccf914e51a4
David Z. Han <zhaohui.han@gmail.com>
2019-10-25 23:15:00 -0400

    Updates

---
+++ precache-manifest.cee961a9b5dc8c396a66301068dcd824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb11ce0f99eed015007ef541dbc72e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9557b2171b29ae66f577",
+    "url": "/static/css/main.1b2f49b9.chunk.css"
+  },
+  {
+    "revision": "da3891e0cc7982bd9af9",
+    "url": "/static/js/2.2296691a.chunk.js"
+  },
+  {
+    "revision": "9557b2171b29ae66f577",
+    "url": "/static/js/main.b849fb36.chunk.js"
+  },
+  {
+    "revision": "ab7c28eeaa53a7987f7a",
+    "url": "/static/js/runtime-main.7cf4b802.js"
+  }
+]);
